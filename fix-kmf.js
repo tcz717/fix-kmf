190cb0303f84fb748b9c131d001b571de8a0326c
Tangent <tcz717@hotmail.com>
2016-10-04 14:36:33 +0800

    Update fix-kmf.js

--- fix-kmf.js
+++ fix-kmf.js
@@ -1,10 +1,10 @@
 define([
-    'jquery', 'https://raw.github.com/aui/artTemplate/master/dist/template.js', 'gre/js/mod/screenword/screen_word'
+    'jquery', 'https://rawgithub.com/aui/artTemplate/master/dist/template.js', 'gre/js/mod/screenword/screen_word'
 ], function (a, b, c) {
     var sc = a('gre/js/mod/screenword/screen_word');
     var d = a('jquery');
     sc.prototype.render = function () {
-        var t = a("https://raw.github.com/aui/artTemplate/master/dist/template.js");
+        var t = a("https://rawgithub.com/aui/artTemplate/master/dist/template.js");
         var b,
             c = d.trim(this.curWord),
             e = c.split(/\s/),
